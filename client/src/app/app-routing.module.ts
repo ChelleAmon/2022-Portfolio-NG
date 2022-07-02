a36e992bb50de2f6a3de88bde91a270adca327fa
Gichelle Amon <amongichelle@gmail.com>
2022-07-01 21:19:05 -0700

    setup lazy loading modules

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -6,7 +6,38 @@ const routes: Routes = [
   {
     path: '',
     component: HomeComponent
-  }
+  },
+  {
+    path: "Hey!",
+    loadChildren: () => import('./modules/landing/landing.module')
+    .then(m => m.LandingModule)
+  },
+  {
+    path: "about",
+    loadChildren: () => import('./modules/about/about.module')
+    .then(m => m.AboutModule)
+  },
+  {
+    path: "skills",
+    loadChildren: () => import('./modules/skills/skills.module')
+    .then(m => m.SkillsModule)
+  },
+  {
+    path: "projects",
+    loadChildren: () => import('./modules/projects/projects.module')
+    .then(m => m.ProjectsModule)
+  },
+  {
+    path: "Contact-me!",
+    loadChildren: () => import('./modules/contacts/contacts.module')
+    .then(m => m.ContactsModule)
+  },
+  {
+    path: "Media-Accounts",
+    loadChildren: () => import('./modules/footer/footer.module')
+    .then(m => m.FooterModule)
+  },
+
 ];
 
 @NgModule({
--- client/src/app/modules/footer/footer-routing.module.ts
+++ client/src/app/modules/footer/footer-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FooterComponent } from './pages/footer/footer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: FooterComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
