9fc02a84ac7989f45f774abe0d47de943d69dc20
Gichelle Amon <amongichelle@gmail.com>
2022-07-01 20:57:14 -0700

    generate footer page component

--- client/src/app/modules/footer/footer.module.ts
+++ client/src/app/modules/footer/footer.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FooterRoutingModule } from './footer-routing.module';
+import { FooterComponent } from './pages/footer/footer.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FooterComponent
+  ],
   imports: [
     CommonModule,
     FooterRoutingModule
