5f10e6c6ed9501ea0710dc14bf70bffcbbe3d877
Gichelle Amon <amongichelle@gmail.com>
2022-07-01 20:54:57 -0700

    generate contacts page component

--- client/src/app/modules/contacts/contacts.module.ts
+++ client/src/app/modules/contacts/contacts.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ContactsRoutingModule } from './contacts-routing.module';
+import { ContactsComponent } from './pages/contacts/contacts.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ContactsComponent
+  ],
   imports: [
     CommonModule,
     ContactsRoutingModule
